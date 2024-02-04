50123be88c147658f85b4c08dfd3d5c05544514d
Natalie Wiggins <islifepeachy@outlook.com>
2024-02-04 18:54:08 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rustup update && \
 rustup target add wasm32-unknown-unknown && \
+rustup update && \
 rustup component add rustfmt --toolchain nightly && \
 cargo install cargo-binstall && \
 cargo binstall trunk leptosfmt
